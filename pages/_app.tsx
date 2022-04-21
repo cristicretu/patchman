1cc9eb8199ee4073564c138f6f63c921bc35c9fe
Cristian Crețu <crisemcr@gmail.com>
2022-04-21 14:27:14 +0300

    [feat]: Req table

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import type { AppProps } from 'next/app'
-import { GeistProvider, CssBaseline } from '@geist-ui/core'
+import type { AppProps } from "next/app";
+import { GeistProvider, CssBaseline } from "@geist-ui/core";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <GeistProvider>
-    <CssBaseline />
-    <Component {...pageProps} />
-  </GeistProvider>
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-import { Button, Input, Select, Tabs, Text } from "@geist-ui/core";
-import Page from "@geist-ui/core";
+import {
+  Button,
+  Divider,
+  Fieldset,
+  Input,
+  Select,
+  Tabs,
+  Text,
+} from "@geist-ui/core";
 import type { NextPage } from "next";
-import PageContent from "@geist-ui/core/esm/page/page-content";
+import PageContent from "@geist-ui/core";
 
 const Home: NextPage = () => {
   const methodHandler = (method: string) => {
@@ -9,45 +16,84 @@ const Home: NextPage = () => {
   };
 
   return (
-    <PageContent>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "24px",
+        padding: "24px",
+      }}
+    >
+      <Text h1>Patchman</Text>
+
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
-          gap: "24px",
-          padding: "24px",
+          alignContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
         }}
       >
-        <Text h1>Patchman</Text>
+        <Select placeholder="GET">
+          <Select.Option value="GET">GET</Select.Option>
+          <Select.Option value="POST">POST</Select.Option>
+          <Select.Option value="PUT">PUT</Select.Option>
+          <Select.Option value="DELETE">DELETE</Select.Option>
+          <Select.Option value="PATCH">PATCH</Select.Option>
+          <Select.Option value="HEAD">HEAD</Select.Option>
+          <Select.Option value="OPTIONS">OPTIONS</Select.Option>
+        </Select>
+        <Input
+          placeholder="https://jsonplaceholder.typicode.com/comments"
+          width="100%"
+          scale={1.13}
+        />
+        <Button type="success" ghost auto>
+          Send
+        </Button>{" "}
+      </div>
 
-        <div
-          style={{
-            display: "flex",
-            alignContent: "center",
-            alignItems: "center",
-            gap: "0.5rem",
-          }}
-        >
-          <Select placeholder="GET">
-            <Select.Option value="GET">GET</Select.Option>
-            <Select.Option value="POST">POST</Select.Option>
-            <Select.Option value="PUT">PUT</Select.Option>
-            <Select.Option value="DELETE">DELETE</Select.Option>
-            <Select.Option value="PATCH">PATCH</Select.Option>
-          </Select>
-          <Input
-            placeholder="https://jsonplaceholder.typicode.com/comments"
-            width="100%"
-            scale={1.13}
-          />
-          <Button type="success" ghost auto>
-            Send
-          </Button>{" "}
-        </div>
-
-        <Tabs initialValue="1">
+      <Fieldset>
+        <Tabs initialValue="1" paddingRight={2}>
           <Tabs.Item label="Query Params" value="1">
-            conteeeeeent query params{" "}
+            <div
+              style={{
+                display: "flex",
+                gap: "0.5rem",
+              }}
+            >
+              <Input
+                label="Key"
+                placeholder="message"
+                clearable
+                width="100%"
+                style={{ flexBasis: "60%", width: "10%" }}
+              />
+              <Input
+                label="Value"
+                placeholder="Hello, World!"
+                clearable
+                width="100%"
+                style={{ flexBasis: "60%", width: "10%" }}
+              />
+              <Button
+                type="error"
+                ghost
+                scale={0.8}
+                style={{ flexBasis: "5%", width: "5%" }}
+              >
+                Delete
+              </Button>
+            </div>
+            <Button
+              type="success"
+              ghost
+              auto
+              style={{ marginTop: "24px" }}
+              scale={0.8}
+            >
+              Add
+            </Button>{" "}
           </Tabs.Item>
           <Tabs.Item label="Headers" value="2">
             Between the Web browser and the server, numerous computers and
@@ -57,8 +103,13 @@ const Home: NextPage = () => {
             JSON
           </Tabs.Item>
         </Tabs>
-      </div>
-    </PageContent>
+      </Fieldset>
+
+      <Fieldset>
+        <Text h2>Response</Text>
+        <Divider my={0} />
+      </Fieldset>
+    </div>
   );
 };
 
